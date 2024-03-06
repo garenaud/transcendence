6d1de06dadc8c8f47b4eef4512fc7e53149a736a
alieufriggeri <alieufriggeri00@gmail.com>
2024-03-06 15:25:37 +0100

    can now register input from user via websocket

--- transcendence/game/consumers.py
+++ transcendence/game/consumers.py
@@ -11,5 +11,17 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-
         self.send(text_data=json.dumps({"message": message}))
+        
+class GameConsumer(WebsocketConsumer):
+	finished = False
+	scoreleft = 0
+	scoreright = 0
+	ball_position_x = 0.0
+	ball_position_z = 0.0
+	ball_velocity_x = 0.0
+	ball_velocity_z = 0.0
+	paddleleft_position_x = 0.0
+	paddleleft_position_z = 0.0
+	paddleright_position_x = 0.0
+	paddleright_position_z = 0.0
