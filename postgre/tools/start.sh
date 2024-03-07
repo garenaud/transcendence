b8ce942744076945d256677d8247d5851b501ba7
Victor Groux <victorgroux99@gmail.com>
2024-03-07 13:55:54 +0100

    little problem with postgres (no actual collation version, but a version was recorded)

--- postgre/tools/start.sh
+++ postgre/tools/start.sh
@@ -7,11 +7,7 @@ echo    $POSTGRES_USER:$POSTGRES_PASSWORD | /usr/sbin/chpasswd
 echo    "CREATE USER $POSTGRES_USER WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';" >> postgres.sql
 echo    "CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;" >> postgres.sql
 echo    "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;" >> postgres.sql
-
+echo	"ALTER DATABASE $POSTGRES_DB REFRESH COLLATION VERSION;" >> postgres.sql
 
 psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -dbname $POSTGRES_DB -f postgres.sql
-
-# cat postgres.sql
-# sudo    -u postgres psql -f postgres.sql
-
-# service postgresql restart
+rm postgres.sql
