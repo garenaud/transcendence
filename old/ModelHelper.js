fbcedc2541d1c8243f93ce5979b941df5b19a4e3
Pierre 42 <pierre.podnieks@gmail.com>
2024-03-14 13:58:54 +0100

    Add proxy configuration for game endpoint

--- old/ModelHelper.js
+++ old/ModelHelper.js
@@ -1,22 +1,22 @@
-// import * as THREE from 'three'
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-// // import {LoadGLTFByPath} from './ModelHelper.js'
+// import {LoadGLTFByPath} from './ModelHelper.js'
 
-// const scenePath = 'assets/scene.gltf'
+const scenePath = 'scene.gltf'
 
-// export const LoadGLTFByPath = (scene) => {
-//     return new Promise((resolve, reject) => {
-//       // Create a loader
-//       const loader = new GLTFLoader();
+export const LoadGLTFByPath = (scene) => {
+    return new Promise((resolve, reject) => {
+      // Create a loader
+      const loader = new GLTFLoader();
   
-//       // Load the GLTF file
-//       loader.load(scenePath, (gltf) => {
-//         scene.add(gltf.scene);
+      // Load the GLTF file
+      loader.load(scenePath, (gltf) => {
+        scene.add(gltf.scene);
 
-//         resolve();
-//       }, undefined, (error) => {
-//         reject(error);
-//       });
-//     });
-// };
+        resolve();
+      }, undefined, (error) => {
+        reject(error);
+      });
+    });
+};
