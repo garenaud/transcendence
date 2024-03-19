a1fc0ec34d0cac790b651242e8e337307171c8c6
Pierre 42 <pierre.podnieks@gmail.com>
2024-03-19 15:35:01 +0100

    Remove unused files and update character controls

--- second game/rungame.js
+++ second game/rungame.js
@@ -3,6 +3,9 @@ import {FBXLoader} from 'three/addons/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
+var Run = [];
+var Idle = [];
+
 class BasicCharacterControls {
   constructor(params) {
     this._Init(params);
@@ -28,20 +31,14 @@ class BasicCharacterControls {
     switch (event.keyCode) {
       case 87: // w
         this._move.forward = true;
-        break;
-      case 65: // a
-        this._move.left = true;
+		if (Run) Run.play();
         break;
       case 83: // s
         this._move.backward = true;
-        break;
-      case 68: // d
-        this._move.right = true;
+		if (Run) Run.play();
         break;
       case 38: // up
-      case 37: // left
       case 40: // down
-      case 39: // right
         break;
     }
   }
@@ -50,15 +47,11 @@ class BasicCharacterControls {
     switch(event.keyCode) {
       case 87: // w
         this._move.forward = false;
-        break;
-      case 65: // a
-        this._move.left = false;
+		if (Run) Run.stop();
         break;
       case 83: // s
         this._move.backward = false;
-        break;
-      case 68: // d
-        this._move.right = false;
+		if (Run) Run.stop();
         break;
       case 38: // up
       case 37: // left
@@ -86,21 +79,11 @@ class BasicCharacterControls {
     const _A = new THREE.Vector3();
     const _R = controlObject.quaternion.clone();
 
-    if (this._move.forward) {
-      velocity.z += this._acceleration.z * timeInSeconds;
+    if (this._move.forward) {		
+		velocity.z += this._acceleration.z * timeInSeconds;
     }
     if (this._move.backward) {
       velocity.z -= this._acceleration.z * timeInSeconds;
-    }
-    if (this._move.left) {
-      _A.set(0, 1, 0);
-      _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * this._acceleration.y);
-      _R.multiply(_Q);
-    }
-    if (this._move.right) {
-      _A.set(0, 1, 0);
-      _Q.setFromAxisAngle(_A, -Math.PI * timeInSeconds * this._acceleration.y);
-      _R.multiply(_Q);
     }
 
     controlObject.quaternion.copy(_R);
@@ -108,11 +91,11 @@ class BasicCharacterControls {
     const oldPosition = new THREE.Vector3();
     oldPosition.copy(controlObject.position);
 
-    const forward = new THREE.Vector3(0, 0, 1);
+    const forward = new THREE.Vector3(0, 0, 0);
     forward.applyQuaternion(controlObject.quaternion);
     forward.normalize();
 
-    const sideways = new THREE.Vector3(1, 0, 0);
+    const sideways = new THREE.Vector3(0, 0, 0);
     sideways.applyQuaternion(controlObject.quaternion);
     sideways.normalize();
 
@@ -207,17 +190,12 @@ class LoadModelDemo {
     this._mixers = [];
     this._previousRAF = null;
 
-    this._LoadAnimatedModel();
-    // this._LoadAnimatedModelAndPlay(
-        // './models/', 'Mexican.fbx', 'Breakdance.fbx', new THREE.Vector3(0, 1, 0));
-    // this._LoadAnimatedModelAndPlay(
-        // './models/', 'TheBoss.fbx', 'Run.fbx', new THREE.Vector3(12, 0, -10));
-    // this._LoadAnimatedModelAndPlay(
-    //     './resources/dancer/', 'dancer.fbx', 'Silly Dancing.fbx', new THREE.Vector3(-12, 0, -10));
+    this._LoadTheBoss();
+	this._LoadTheCatch();
     this._RAF();
   }
 
-  _LoadAnimatedModel() {
+  _LoadTheBoss() {
     const loader = new FBXLoader();
     loader.setPath('./models/');
     loader.load('TheBoss.fbx', (fbx) => {
@@ -225,68 +203,73 @@ class LoadModelDemo {
       fbx.traverse(c => {
         c.castShadow = true;
       });
-	  const offset = new THREE.Vector3(0, -11, 0);
+	  const offset = new THREE.Vector3(0, 0, 0);
 	  fbx.position.copy(offset);
-	//   const quaternion = new THREE.Vector4(10, 10, 10, 10);
-	//   fbx.quaternion.set(10, 10, 10, 10);
-	//   console.log(fbx.quaternion);
-
       const params = {
         target: fbx,
         camera: this._camera,
       }
       this._controls = new BasicCharacterControls(params);
 
-      const anim = new FBXLoader();
+	  const anim = new FBXLoader();
       anim.setPath('./models/');
-      anim.load('Twerk.fbx', (anim) => {
+      anim.load('DwarfIdle.fbx', (anim) => {
         let m = new THREE.AnimationMixer(fbx);
         this._mixers.push(m);
-        const idle = m.clipAction(anim.animations[0]);
+        Idle = m.clipAction(anim.animations[0]);
 		console.log(anim.animations[0]);
-        idle.play();
-      });
-	//   anim.load('Run.fbx', (anim) => {
-	// 		let m = new THREE.AnimationMixer(fbx);
-	// 		this._mixers.push(m);
-	// 		const idle = m.clipAction(anim.animations[1]);
-	// 		// idle.play();
-	//   });
-      this._scene.add(fbx);
+		Idle.play();
+	});
+	const Walkanim = new FBXLoader();
+	console.log('allo');
+	Walkanim.setPath('./models/');
+	Walkanim.load('Run.fbx', (Walkanim) => {
+		let RunAction = new THREE.AnimationMixer(fbx);
+		this._mixers.push(RunAction);
+		Run = RunAction.clipAction(Walkanim.animations[0]);
+		console.log(Walkanim.animations[0]);
+	});
+      	this._scene.add(fbx);
     });
   }
 
-//   _LoadAnimatedModelAndPlay(path, modelFile, animFile, offset) {
-//     const loader = new FBXLoader();
-//     loader.setPath(path);
-//     loader.load(modelFile, (fbx) => {
-//       fbx.scale.setScalar(0.1);
-//       fbx.traverse(c => {
-//         c.castShadow = true;
-//       });
-//       fbx.position.copy(offset);
+  _LoadTheCatch() {
+    const loader = new FBXLoader();
+    loader.setPath('./models/');
+    loader.load('Catch.fbx', (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse(c => {
+        c.castShadow = true;
+      });
+	  const offset = new THREE.Vector3(20, -1, 0);
+	  fbx.position.copy(offset);
+      const params = {
+        target: fbx,
+        camera: this._camera,
+      }
+      this._controls = new BasicCharacterControls(params);
 
-//       const anim = new FBXLoader();
-//       anim.setPath(path);
-//       anim.load(animFile, (anim) => {
-//         const m = new THREE.AnimationMixer(fbx);
-//         this._mixers.push(m);
-//         const idle = m.clipAction(anim.animations[0]);
-//         idle.play();
-//       });
-//       this._scene.add(fbx);
-//     });
-//   }
-
-//   _LoadModel() {
-//     const loader = new GLTFLoader();
-//     loader.load('./resources/thing.glb', (gltf) => {
-//       gltf.scene.traverse(c => {
-//         c.castShadow = true;
-//       });
-//       this._scene.add(gltf.scene);
-//     });
-//   }
+	  const anim = new FBXLoader();
+      anim.setPath('./models/');
+      anim.load('DwarfIdle.fbx', (anim) => {
+        let m = new THREE.AnimationMixer(fbx);
+        this._mixers.push(m);
+        Idle = m.clipAction(anim.animations[0]);
+		console.log(anim.animations[0]);
+		Idle.play();
+	});
+	const Walkanim = new FBXLoader();
+	console.log('allo');
+	Walkanim.setPath('./models/');
+	Walkanim.load('Run.fbx', (Walkanim) => {
+		let RunAction = new THREE.AnimationMixer(fbx);
+		this._mixers.push(RunAction);
+		Run = RunAction.clipAction(Walkanim.animations[0]);
+		console.log(Walkanim.animations[0]);
+	});
+      	this._scene.add(fbx);
+    });
+  }
 
   _OnWindowResize() {
     this._camera.aspect = window.innerWidth / window.innerHeight;
