d605c399e5e0934fa84d743b083709345ff5d325
garenaud <gaetan.renaud2300@gmail.com>
2024-05-14 13:00:30 +0200

    Erreur 404 en cas de mauvais url, urlHistory (array cree pour retracer l'historique) est je crois ok

--- frontend/srcs/components/stateManager.js
+++ frontend/srcs/components/stateManager.js
@@ -12,6 +12,8 @@ import { renderScratchGame } from './scratchGame.js';
 import { createToastComponent, createButtonComponent, createPhotoComponent, createListCardComponent, renderDiv } from './globalComponent.js';
 import { showUserList, showGameList } from './listComponent.js';
 
+let currentIndex = -1;
+
 // Initialisation de l'état de l'application et du current user
 export let appState = {
     currentView: 'login',
@@ -20,29 +22,23 @@ export let appState = {
     users: [],
     urlHistory: ['login'],
     renderedComponents: {},
-    language: 'fr'
+    language: 'fr',
+    newViewAdded: false
 };
 
 // Fonction pour changer la vue actuelle de l'application
 export function changeView(newView) {
     if (appState.currentView !== newView) {
         appState.currentView = newView;
-        if (appState.urlHistory[appState.urlHistory.length - 1] !== newView) { // Ajoutez cette condition
-            appState.urlHistory.push(newView);
-        }
         // Supprimez les composants de la vue précédente de appState.renderedComponents
         for (let component in appState.renderedComponents) {
             if (component.startsWith(appState.currentView)) {
                 delete appState.renderedComponents[component];
             }
         }
-        sessionStorage.setItem('renderedComponents', JSON.stringify(appState.renderedComponents));
     }
+    appState.newViewAdded = true;
     location.hash = newView;
-    appState.currentView = newView;
-    if (appState.urlHistory[appState.urlHistory.length - 1] !== newView) { // Ajoutez cette condition
-        appState.urlHistory.push(newView);
-    }
 }
 
 // Écouteur d'événement pour changer la vue lorsque l'URL change (rajoute le # à l'URL lorsqu'on change de vue)
@@ -55,26 +51,49 @@ window.addEventListener("hashchange", function() {
 });
 
 // Fonction pour que l'historique du navigateur fonctionne correctement avec les vues de l'application (popstate se declenche lorsque l'on presse sur precedent)
-window.addEventListener("popstate", async function() {
+window.addEventListener("popstate", function() {
     const newView = location.hash.substring(1);
+    const newIndex = appState.urlHistory.lastIndexOf(newView);
+
     if (newView === 'login' && appState.urlHistory.length === 2) {
-        //const translations = await fetch('../json/' + appState.language + '.json').then(response => response.json());
-        //const confirmLogout = window.confirm(translations.confirmLogout)
         const confirmLogout = window.confirm('Si vous revenez à cette page, vous serez déconnecté. Êtes-vous sûr de vouloir continuer ?');
         if (confirmLogout) {
-            // Déconnectez l'utilisateur et mettez à jour l'état de l'application
-            appState.user = null;
-            appState.currentView = newView;
-            appState.urlHistory.pop();
             console.log('bye bye mon ami tu as choisi de nous quitter!!!!');
         } else {
-            // Annulez l'action précédente
             history.pushState(null, null, '#' + appState.urlHistory[appState.urlHistory.length - 1]);
         }
-    } else {
+    } else if (appState.newViewAdded) {
+        // Une nouvelle vue a été ajoutée à l'historique, donc l'utilisateur n'a pas appuyé sur Précédent
+        console.log("T'AS CLIQUE SUR UN LIEN");
         appState.currentView = newView;
+        if (appState.urlHistory[appState.urlHistory.length - 1] !== newView) {
+            appState.urlHistory.push(newView);
+        }
+        currentIndex++;
+        appState.newViewAdded = false;
+    } else if (newIndex < currentIndex) {
+        console.log("T'AS PRESSE SUR SUIVANT");
+        appState.currentView = newView;
+        if (appState.urlHistory[appState.urlHistory.length - 1] !== newView) {
+            appState.urlHistory.push(newView);
+        }
+        currentIndex++;
+    } else {
+        console.log("T'AS PRESSE SUR PRECEDENT");
         appState.urlHistory.pop();
-        console.log("!!!!!!!!!!!!!!!!!!!!! urlHistory = ", appState.urlHistory);
+        currentIndex--;
+    }
+
+    console.log("!!!!!!!!!!!!!!!!!!!!! urlHistory = ", appState.urlHistory);
+});
+
+window.addEventListener("pushstate", function() {
+    const newView = location.hash.substring(1);
+    if (appState.currentView !== newView) {
+        appState.currentView = newView;
+        if (appState.urlHistory[appState.urlHistory.length - 1] !== newView) {
+            appState.urlHistory.push(newView);
+        }
     }
 });
 
@@ -84,8 +103,9 @@ export function getCurrentView() {
 
 // Fonction principale pour rendre l'application en fonction de l'état actuel
 export async function renderApp() {
-    if (!location.hash) { //) {
+    if (!location.hash || appState.userId == 0) { //) {
         location.hash = '#login';
+		// console.log(appState.userId);
         //appState.urlHistory.push('login');
 /*         await renderApp();
         return; */
@@ -98,11 +118,18 @@ export async function renderApp() {
         appState.currentView = ['login', 'hero', 'game', 'chat'].includes(view) ? view : 'login';
         appState.language = 'fr';
     }
+    const validViews = ['login', 'game', 'hero'];
     appState.currentView = location.hash.substring(1) || 'login';
     document.body.innerHTML = '';
+    if (!validViews.includes(appState.currentView)) {
+        // Redirigez vers une page d'erreur 404
+        document.body.innerHTML = '<h1>Erreur 404 : Page non trouvée</h1>';
+        return;
+    }
     if (!appState.renderedComponents) {
         appState.renderedComponents = {};
     }
+	console.log(appState.currentView);
     switch(appState.currentView) {
         case 'login':
             if (!appState.renderedComponents.login) {
@@ -132,32 +159,30 @@ export async function renderApp() {
                         appState.renderedComponents.navbar = true;
                     }
                     break;
-                case 'game':
-                    console.log(document.querySelector('.navbar-expand-lg'));
-                    console.log("appstate dans game: ", appState);
-                    loadLanguage(appState.language);
-                    if (!appState.renderedComponents.game) {
-                        await LanguageBtn();
+                    case 'game':
+                        console.log(document.querySelector('.navbar-expand-lg'));
+                        console.log("appstate dans game: ", appState);
                         loadLanguage(appState.language);
-                        renderNavbar(appState.user);
-                        const game = await renderPong();
-                        const game2 = await renderRun();
-                        const gameListHTML = await showGameList();
-                        const cardHistory = createListCardComponent('pongPlayed', 'Games', gameListHTML);
-                        await renderDiv([cardHistory, game], 'row');
-                        await LanguageBtn();
-                        // renderNavbar(appState.user);
-                        appState.renderedComponents.game = true;
-                        appState.renderedComponents.navbar = true;
-                    } else {
-                        renderNavbar(appState.user);
-                    }
-                    break;
-                case 'chat':
-                    const chat = await renderChat();
-                    await renderDiv([chat], 'chat-div');
-                    renderNavbar(appState.user);
-                    break;
+                        if (!appState.renderedComponents.game) {
+                            await LanguageBtn();
+                            loadLanguage(appState.language);
+                            if(!document.querySelector('.navbar')){
+                                renderNavbar(appState.user);
+                            }
+                            const game = await renderPong();
+                            const game2 = await renderRun();
+                            const gameListHTML = await showGameList();
+                            const cardHistory = createListCardComponent('pongPlayed', 'Games', gameListHTML);
+                            await renderDiv([cardHistory, game], 'row');
+                            await LanguageBtn();
+                            // renderNavbar(appState.user);
+                            appState.renderedComponents.game = true;
+                            appState.renderedComponents.navbar = true;
+                        } 
+                        // else {
+                        //     renderNavbar(appState.user);
+                        // }
+                        break;
             }
         break;
     }
