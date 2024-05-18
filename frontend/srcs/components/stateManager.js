c1e510cdc0c9a3e332a82ad3babeef57da0808cc
Pedroamiguel <pierre.podnieks@gmail.com>
2024-05-18 12:02:44 +0200

    Fixed pop-up when login

--- frontend/srcs/components/stateManager.js
+++ frontend/srcs/components/stateManager.js
@@ -51,16 +51,12 @@ export function changeView(newView) {
 window.addEventListener("hashchange", function () {
     const newView = location.hash.substring(1);
     if (appState.currentView !== newView) {
-        appState.currentView = newView;
+		appState.currentView = newView;
     }
-    const currentUser = getCurrentUser();
-    if (!currentUser && newView !== 'login') {
+    if (!appState.user && newView !== 'login') {
         window.location.hash = 'login';
         return;
     }
-    // if (appState.urlHistory[appState.urlHistory.length - 1] !== newView) {
-    //     appState.urlHistory.push(newView);
-    // }
     renderApp();
 });
 
@@ -70,7 +66,7 @@ window.addEventListener("popstate", function () {
     const newIndex = appState.urlHistory.lastIndexOf(newView);
     const currentUser = getCurrentUser();
     console.log("currentUser popstate = ", currentUser);
-    if (!currentUser && newView !== 'login') {
+    if (!appState.user && newView !== 'login') {
         window.location.hash = 'login';
         return;
     }
